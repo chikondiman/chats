d16efe75dbee54093296e2fa995a0f96653b653e
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-16 11:10:27 -0400

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 export default function Dashboard() {
     return (
