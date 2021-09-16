8f4c18c83262f14a5077e7c314c1bf0f3d6ef2ae
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-16 11:16:31 -0400

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,10 +6,12 @@ import { mergeClasses } from '@material-ui/styles';
 export default function Dashboard() {
     return (
         <div>
+            <p> yo</p>
             <Paper className={mergeClasses.root}>
                 <Typography variant="h5" component="h3">
                     This is a shet of paper
                 </Typography>
+
 
             <Typography component="p">
                 Paper can be used to build surface
