be58a06f70a682117d824adcf43cc0a8617fe9fe
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-16 11:11:37 -0400

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import { mergeClasses } from '@material-ui/styles';
 
 export default function Dashboard() {
     return (
         <div>
+            <Paper className={mergeClasses.root}>
+                <Typogrphy variant="h5" component="h3">
+                    This is a shet of paper
+                </Typogrphy>
+            </Paper>
         
         </div>
     )
