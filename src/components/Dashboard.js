40720c18edb5af2f41ed0f0879da485c20b43d7f
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-17 13:15:02 -0400

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,7 +7,8 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Chip from '@material-ui/core/Chip';
-import Button from 'material-ui/core/Button'
+import Button from 'material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
     root: {
