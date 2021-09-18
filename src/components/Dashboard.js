a66fb9112445848ed8705df0db02b615635f5d34
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-18 10:58:34 -0500

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -96,15 +96,7 @@ export default function Dashboard() {
 
             <div className={classes.flex}>
             
-            <TextField
-             id="standard-name"
-             label="Send a chat"
-             className={classes.chatBox}
-             value={textValue}
-             onChange={e => changeTextValue(e.target.value)}
-             margin="normal"
-             padding="10px"/>
-             
+     
 
             <Button variant="contained" color="primary">
                     Send
