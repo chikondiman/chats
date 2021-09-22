bcd2015cf6b461a8cacb16f3399ce984756f5a39
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-22 09:23:45 -0400

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,14 @@ app.get('/', function(req,res){
     res.send('<h1>Hello chats</h1>');
 });
 
+io.on('connection', function(socket){
+console.log('a user connected');
+socket.on('chat message', function(msg){
+    console.log('message: ' + JSON.stringify(msg));
+    io.emit('chat message', msg);
+});
+});
+
 http.listen(3001, function(){
     console.log('listening on *:3001');
 });
