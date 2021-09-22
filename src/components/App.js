a329cb97b9ab85a15c1d258bc6e771032174b2f8
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-22 09:23:34 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import React from 'react';
 import Dashboard from './Dashboard.js'
+import Store from './Store.js'
 
 function App() {
   return (
     <div className="App">
+      <Store>
       <Dashboard/>
-
+      </Store>
     </div>
   );
 }
